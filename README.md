20b9c86227c350773893831aacd65783528926ec
donovanolivarez <donovanolivarez97@gmail.com>
2025-07-23 00:16:06 -0500

    range table function, readme updates, samples added

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Audio Steganography
- 
+Team #20 Feedback
+--- i know a lot of papers say convert secret message to a bitstream. All that
+means is that we read the file into memory and access it one (or multiple) bits at
+a time.
+
+--- read cover. read message. don't need the "loop" step since the line going back
+up signifies a loop
+
+--- instead of "hide specified number of bits" say what you actually need to do. I
+suspect you have to add/sub from either left/right samples so that the difference
+encodes the message bits.
+
+--- given that you will have to change, whenever the difference is zero, that could
+encode a zero bit, otherwise you can adjust to make the difference a 1. So there
+don't need to be any invalid differences where you hide none
+
+--- make sure when you make adjustments that the new difference is in the same
+range as the old difference
+
+--- also have a check if there are more samples - if not, output "message not fully
+hidden" or something like that - either way, save the output file
+
+--- for the range, i suspect you'll take the floor{log2( |difference| )} to get
+number of bits
+so if a range is 8 to 15, then you can hide 3 bits and still stay in that range.
+8=1000 in binary, 15=1111, so you can change the 3 bits to anything and still be in
+the range.
+
+--- here is the fun part, allow for a "fudge factor" so the encoder can jump
+ranges. if the fudge factor is 0, you can't change. if it's one, you can jump up
+one range. If the difference is 15, you could hide 4 bits, BUT you must make sure
+the new difference will be in the 4-bit range
+
+--- ultimately, we want to be sure that you can hide enough data TO be audible so
+that we can test the limits (capacity/perceptibility) of this technique - the fudge
+factor would be a user input
+
+--- "Audacity" is a free program and great for working with audio file
+
+
+## TODO:
+- Range Table Generation (Sort of done, but may need a bit more testing)
+- Encoder
+- Decoder
+- Argparse to make it a command line program with args https://docs.python.org/3/library/argparse.html
+- Testing of Hiding and Extraction
+- Analysis
+- Report
---
+++ main.py
@@ -0,0 +1,161 @@
+from scipy.io import wavfile
+from numpy import ndarray
+import math
+
+
+
+# ------ Range Table Functions ------ #
+# we can control how fast the ranges grow with a cmd parameter. This can show a capacity vs stealth tradeoff.
+# for now, hardcoded.
+def generate_range_table():
+    """
+    generate a range table. I don't think any input should be needed here yet.
+    """
+    range_table = []
+
+    MAX_DIFF = 65536
+
+    # create range table entries until we hit max diff
+    current = 1
+
+    # can this be an arg? 
+    range_size = 4
+
+    while current <= MAX_DIFF:
+
+        for _ in range(16):
+
+            # start of the range for the entry
+            start = current
+
+            # getting the value for the end of the range for this entry.
+            end = min(start + range_size - 1, MAX_DIFF)
+
+            current_entry_size = end - start + 1
+
+            if range_size <= current_entry_size:
+                # normal case, we can create a full size entry for the table.
+
+                # we need three things for the table:
+                # start, end, # of bits, actual bit sequence.
+                num_bits = math.log2(range_size)
+
+                range_table.append(
+                    {
+                        "start": start,
+                        "end": end,
+                        "num_bits": num_bits,
+                        "bits": None
+                    }
+                )
+
+                current = end + 1
+
+            elif current_entry_size > 0:
+                # we can't create a full range size, so find the correct size that we CAN hide.
+                num_bits = math.floor(math.log2(current_entry_size))
+
+                if num_bits > 0:
+                    power_of_2_size = 2**num_bits
+                    end = start + power_of_2_size - 1
+
+                    range_table.append({
+                        'start': start,
+                        'end': end,
+                        'num_bits': num_bits,
+                        'bits': None
+                    })
+                    current = end + 1
+
+            else:
+                break
+
+        range_size *= 2
+
+    # handle negative ranges
+    negative_table = []
+    for entry in reversed(range_table):
+        negative_table.append({
+            'start': -entry['end'],
+            'end': -entry['start'],
+            'num_bits': entry['num_bits'],
+            'bits': None
+        })
+        
+    # zero range
+    zero_range = [{
+        'start': 0,
+        'end': 0,
+        'num_bits': 0, # Cannot hide data
+        'bits': None
+    }]
+
+    return negative_table + zero_range + range_table
+
+
+def generate_bit_sequences(range_table):
+    counters = {}
+
+    for entry in range_table:
+        num_bits = entry['num_bits']
+
+        if num_bits == 0:
+            continue
+
+        # If we see this bit length for the first time, initialize its counter
+        if num_bits not in counters:
+            counters[num_bits] = 0
+        
+        # Get the current ID and format it as a binary string with leading zeros
+        current_id = counters[num_bits]
+        bit_string = f'{current_id:0{int(num_bits)}b}'
+        
+        # Assign the bit string to the table entry
+        entry['bits'] = bit_string
+        
+        # Increment the counter for the next range of this size
+        counters[num_bits] += 1
+        
+    return range_table
+
+        
+
+table = generate_range_table()
+full_range_table = generate_bit_sequences(range_table=table)
+
+# check some of the data in the range table
+for i in range(10000):
+    print(full_range_table[i])
+
+
+# ------ Reading and Writing Functions ------ #
+def read_wav(sample: str) -> ndarray:
+    """
+    read a wav file. Should also add some validation here to make sure we are using the right cover files.
+    """
+
+    # 2d array, each row is one sample. the first column has values from the left channel, the second column from the right channel.
+    samplerate, data = wavfile.read(sample)
+
+    print(data.shape[0] / samplerate)
+
+
+    # this doesn't really matter right now.
+    for sample in data:
+        left = sample[0]
+        right = sample[1]
+        print(sample)
+
+    return data, samplerate
+
+
+def write_wav(wav_file_data: ndarray, samplerate: int) -> None:
+    """
+    writes the final output. It's unclear if this needs to be a seperate method yet.
+    """
+    wavfile.write(filename='output.wav', rate=samplerate, data=wav_file_data)
+
+
+# uncomment to read a sample and immediately write it back. It should be the exact same sample. You'll need to change the filename.
+# wav_file_data, samplerate = read_wav('sample1.wav')
+# write_wav(wav_file_data=wav_file_data, samplerate=samplerate)
